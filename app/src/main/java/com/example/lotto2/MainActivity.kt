6ec718d54c06424bde90282aeccc765c41fb1082
KenLin03330 <a0932511985@gmail.com>
2024-03-06 18:11:04 +0800

    加入樂透功能

--- app/src/main/java/com/example/lotto2/MainActivity.kt
+++ app/src/main/java/com/example/lotto2/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.lotto2
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.TextView
+import android.view.View
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -10,5 +11,16 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         var t: TextView = findViewById(R.id.txv)
         t.text = "0"
+
+    }
+
+    fun happy(v: View) {
+        var txv: TextView = findViewById(R.id.txv)
+        if (v.id == R.id.btn) {
+            txv.text = (1..100).random().toString()
+
+        } else {
+            txv.text = "0"
+        }
     }
 }
