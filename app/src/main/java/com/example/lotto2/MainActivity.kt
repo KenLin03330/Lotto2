b2fa69d11f0f410a2d17296f7b93f8a368f3a41d
KenLin03330 <a0932511985@gmail.com>
2024-03-06 16:54:53 +0800

    第二次commit (修改文字元件)

--- app/src/main/java/com/example/lotto2/MainActivity.kt
+++ app/src/main/java/com/example/lotto2/MainActivity.kt
@@ -2,10 +2,13 @@ package com.example.lotto2
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        var t: TextView = findViewById(R.id.txv)
+        t.text = "0"
     }
 }
